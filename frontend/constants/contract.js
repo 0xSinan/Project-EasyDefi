0f9aa1ca1361d610c4854a9d21ae7cc62d0304aa
0xSinan <sinan.formation@gmail.com>
2024-12-28 16:02:28 +0100

    parse ABI on frontend for vercel env var

--- frontend/constants/contract.js
+++ frontend/constants/contract.js
@@ -1,6 +1,6 @@
 export const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
-export const contractAbi = process.env.NEXT_PUBLIC_CONTRACT_ABI;
-export const erc20Abi = process.env.NEXT_PUBLIC_ERC20_ABI;
+export const contractAbi = JSON.parse(process.env.NEXT_PUBLIC_CONTRACT_ABI);
+export const ERC20Abi = JSON.parse(process.env.NEXT_PUBLIC_ERC20_ABI);
 export const TOKENS = {
   ALYRA: {
     address: process.env.NEXT_PUBLIC_ALYRA_ADDRESS,
